a801985c81b7c5231a48d41ba59b092c0f43a813
Fábio Abreu <fabio.abreu@robozz.com.br>
2025-06-06 13:46:02 -0300

    created a container for the table

--- projects/frontek/src/lib/grids/grid/grid.component.ts
+++ projects/frontek/src/lib/grids/grid/grid.component.ts
@@ -11,29 +11,37 @@ export class GridComponent {
 
   @Input() headers: { field: string, header: string }[] = [];
   @Input() data: Record<string, any>[] = [];
-  @Input() headerBackgroundColor: string = '#28313E';
-  @Input() bodyBackgroundColor: string = '#182230';
+  @Input() headerBackgroundColor: string = '#2F3845';
+  @Input() bodyBackgroundColor: string = '#1D2634';
   @Input() textColor: string = '#fff';
-  @Input() textFontSize: string = '1rem';
+  @Input() textFontSize: string = '14px';
   @Input() textAlign: string = 'left';
 
 
   tableStyles: Record<string, string> = {};
   thStyles: Record<string, string> = {};
   tdStyles: Record<string, string> = {};
+  containerStyles: Record<string, string> = {};
 
     ngOnChanges() {
       this.tableStyles = {
         'font-size': this.textFontSize,
         'color': this.textColor,
         'text-align': this.textAlign,
-        'font-family': '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+        'font-family': 'IBM Plex Sans, sans-serif',
       };
       this.thStyles = {
         'background-color': this.headerBackgroundColor,
+        'border-bottom': `1px solid ${this.bodyBackgroundColor}`,
+
       };
       this.tdStyles = {
+        'border-bottom': `1px solid ${this.headerBackgroundColor}`,
+      };
+      this.containerStyles = {
+        'width': '100%',
         'background-color': this.bodyBackgroundColor,
+        'border-radius': '8px',
       };
   }
 
